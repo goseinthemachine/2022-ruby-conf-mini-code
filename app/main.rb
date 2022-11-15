20c00d81fb8a9f99978e136f070549fd15f3bdd5
Cameron Gose <cameron.gose@live.com>
2022-11-14 22:08:35 -0500

    add slide 8.2.2

--- app/main.rb
+++ app/main.rb
@@ -10,21 +10,50 @@ def render_grid args
   x_axis = args.grid.w / GRID_SIZE                                                                                                                                                                                                                                     
   y_axis = args.grid.h / GRID_SIZE                                                                                                                                                                                                                                     
   x_axis.each_with_index do |x|                                                                                                                                                                                                                                        
-    args.outputs.lines <<                                                                                                                                                                                                                                              
-        {x: x * GRID_SIZE, y: 0, x2: x * GRID_SIZE, y2: args.grid.h}                                                                                                                                                                                                   
-  end                                                                                                                                                                                                                                                                  
-                                                                                                                                                                                                                                                                        
-  y_axis.each_with_index do |y|                                                                                                                                                                                                                                        
-    args.outputs.lines <<                                                                                                                                                                                                                                              
-      {x: 0, y: y * GRID_SIZE, x2: args.grid.w, y2: y * GRID_SIZE}                                                                                                                                                                                                     
-  end                                                                                                                                                                                                                                                                  
+    args.outputs.lines <<
+        {
+          x: x * GRID_SIZE, 
+          y: 0, 
+          x2: x * GRID_SIZE, 
+          y2: args.grid.h
+        }
+  end
+
+  y_axis.each_with_index do |y|
+    args.outputs.lines <<
+      {
+        x: 0, 
+        y: y * GRID_SIZE, 
+        x2: args.grid.w, 
+        y2: y * GRID_SIZE
+      }
+  end
+end
+
+def render_snake args
+  args.outputs.solids << args.state.head
 end
 
 def render args
   render_grid args
+  render_snake args
+end
+
+def defaults args
+  args.state.head ||=
+  {
+    x: args.grid.w / 2,
+    y: args.grid.h / 2,
+    w: GRID_SIZE,
+    h: GRID_SIZE,
+    r: 23,
+    g: 245,
+    b: 23,
+  }
 end
 
 def tick args
+  defaults args
   handle_input args
   update args
   render args
